64e688856e723262ba25bd22c05914e40bdf530d
Tavis Weeden <tavisweeden@gmail.com>
2019-10-29 00:37:32 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,10 +16,9 @@ def monopoly_with_second_tier
 	monopoly = {:railroads => {:pieces => 4}}
 
 end
-require 'pry'
+
 def monopoly_with_third_tier
 	monopoly = {:railroads => {:pieces => 4}, :names => {}}
-binding.pry
 end
 
 def monopoly_with_fourth_tier
