aab5bbee2b149a5212bc19f526341f3b10780f02
Tavis Weeden <tavisweeden@gmail.com>
2019-10-29 00:34:50 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,8 +21,8 @@ def monopoly_with_third_tier
 	monopoly = {:railroads => {:pieces => 4}, :names => {}}
 
 end
-
+require 'pry'
 def monopoly_with_fourth_tier
 	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
-
+binding.pry
 end
